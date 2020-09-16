a485aa72b7141d69ade84dc9af68c947e9a0c1f3
Carlo Barazzetta <carlo.barazzetta@ethea.it>
2020-09-16 19:23:06 +0200

    *ver.2.3.1

--- Source/IconFontsImageListBase.pas
+++ Source/IconFontsImageListBase.pas
@@ -57,7 +57,7 @@ resourcestring
   ERR_COLLECTION_NOT_ASSIGNED = 'Error: image collection of "%s" not assigned!';
 
 const
-  IconFontsImageListVersion = '2.3.0';
+  IconFontsImageListVersion = '2.3.1';
   DEFAULT_SIZE = 16;
 
 type
